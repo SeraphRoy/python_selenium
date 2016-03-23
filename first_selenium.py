0fd518c4ae0eac264586d12f763966498f644f0d
Claustrophobia <yanxi@umail.ucsb.edu>
2016-03-22 22:28:10 -0700

    ...

--- first_selenium.py
+++ first_selenium.py
@@ -3,7 +3,7 @@ f = open('.password', 'r')
 readPassword = f.read()
 
 browser = webdriver.Chrome()
-isSuccessful = False
+isNotSuccessful = False
 
 def SwitchTo(input):
     SwitchTo = browser.find_element_by_id(input)
@@ -15,7 +15,7 @@ def SwitchTo(input):
     else:
         return True
 
-while 1:
+while 1 and isNotSuccessful:
     browser.get('https://my.sa.ucsb.edu/gold/login.aspx')
     NetId = browser.find_element_by_id('pageContent_userNameText')
     password = browser.find_element_by_id('pageContent_passwordText')
@@ -23,10 +23,10 @@ while 1:
     NetId.send_keys('yanxi')
     password.send_keys(readPassword)
     login.click()
-    MyClassSchedule = browser.find_element_by_id('ct105')
+    MyClassSchedule = browser.find_element_by_xpath("//*[@id='ctl05']")
     MyClassSchedule.click()
     Switch154 = browser.find_element_by_id('pageContent_CourseList_SwitchCourseButton_1')
     Switch154.click()
-    isSuccessful = SwitchTo('pageContent_ClassScheduleUC_dl_coursegroup_PrimarySections_0_SecondarySections_0_SwitchLinkSecondary_2') or SwitchTo('pageContent_ClassScheduleUC_dl_coursegroup_PrimarySections_0_SecondarySections_0_SwitchLinkSecondaryAlt_3')
+    isNotSuccessful = SwitchTo('pageContent_ClassScheduleUC_dl_coursegroup_PrimarySections_0_SecondarySections_0_SwitchLinkSecondary_2') or SwitchTo('pageContent_ClassScheduleUC_dl_coursegroup_PrimarySections_0_SecondarySections_0_SwitchLinkSecondaryAlt_3')
     logout = browser.find_element_by_xpath("//*[@id='headerTable']/tbody/tr[2]/td[2]/a")
     logout.click()
