f7786c8daa117a2987872fcc94d5e8b3c9c8663b
Claustrophobia <yanxi@umail.ucsb.edu>
2016-03-22 22:19:14 -0700

    not done with switching

--- first_selenium.py
+++ first_selenium.py
@@ -3,7 +3,18 @@ f = open('.password', 'r')
 readPassword = f.read()
 
 browser = webdriver.Chrome()
-isSelected = False
+isSuccessful = False
+
+def SwitchTo(input):
+    SwitchTo = browser.find_element_by_id(input)
+    SwitchTo.click()
+    try:
+        Error =  browser.find_element_by_id('pageContent_ClassScheduleUC_ErrorMessageTable')
+    except NoSuchElementException:
+        return False
+    else:
+        return True
+
 while 1:
     browser.get('https://my.sa.ucsb.edu/gold/login.aspx')
     NetId = browser.find_element_by_id('pageContent_userNameText')
@@ -12,17 +23,10 @@ while 1:
     NetId.send_keys('yanxi')
     password.send_keys(readPassword)
     login.click()
-    FindCourses = browser.find_element_by_id('ctl07')
-    FindCourses.click()
-    quarter = browser.find_element_by_xpath("//*[@id='pageContent_quarterDropDown']/option[1]")
-    quarter.click()
-    subject = browser.find_element_by_xpath("//*[@id='pageContent_subjectAreaDropDown']/option[18]")
-    subject.click()
-    beginSearch = browser.find_element_by_id('pageContent_searchButton')
-    beginSearch.click()
-    CS177Section1 = browser.find_element_by_id('pageContent_CourseList_PrimarySections_15_SecondarySections_0_AddLinkSecondary_0')
-    if CS177Section1.get_attribute("class") != "aspNetDisabled":
-        CS177Section1.click()
-        isSelected = True
+    MyClassSchedule = browser.find_element_by_id('ct105')
+    MyClassSchedule.click()
+    Switch154 = browser.find_element_by_id('pageContent_CourseList_SwitchCourseButton_1')
+    Switch154.click()
+    isSuccessful = SwitchTo('pageContent_ClassScheduleUC_dl_coursegroup_PrimarySections_0_SecondarySections_0_SwitchLinkSecondary_2') or SwitchTo('pageContent_ClassScheduleUC_dl_coursegroup_PrimarySections_0_SecondarySections_0_SwitchLinkSecondaryAlt_3')
     logout = browser.find_element_by_xpath("//*[@id='headerTable']/tbody/tr[2]/td[2]/a")
     logout.click()
