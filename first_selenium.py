9b075d000b73c4ddb319c210fcbfb072fbf7bde8
Claustrophobia <yanxi@umail.ucsb.edu>
2016-03-22 22:52:12 -0700

    now work for switching

--- first_selenium.py
+++ first_selenium.py
@@ -3,7 +3,8 @@ f = open('.password', 'r')
 readPassword = f.read()
 
 browser = webdriver.Chrome()
-isNotSuccessful = False
+is154NotSuccessful = True
+is165NotSuccessful = True
 
 def SwitchTo(input):
     SwitchTo = browser.find_element_by_id(input)
@@ -15,7 +16,7 @@ def SwitchTo(input):
     else:
         return True
 
-while 1 and isNotSuccessful:
+while 1 and (is154NotSuccessful or is165NotSuccessful):
     browser.get('https://my.sa.ucsb.edu/gold/login.aspx')
     NetId = browser.find_element_by_id('pageContent_userNameText')
     password = browser.find_element_by_id('pageContent_passwordText')
@@ -25,8 +26,18 @@ while 1 and isNotSuccessful:
     login.click()
     MyClassSchedule = browser.find_element_by_xpath("//*[@id='ctl05']")
     MyClassSchedule.click()
-    Switch154 = browser.find_element_by_id('pageContent_CourseList_SwitchCourseButton_1')
-    Switch154.click()
-    isNotSuccessful = SwitchTo('pageContent_ClassScheduleUC_dl_coursegroup_PrimarySections_0_SecondarySections_0_SwitchLinkSecondary_2') or SwitchTo('pageContent_ClassScheduleUC_dl_coursegroup_PrimarySections_0_SecondarySections_0_SwitchLinkSecondaryAlt_3')
+    if is154NotSuccessful:
+        Switch154 = browser.find_element_by_id('pageContent_CourseList_SwitchCourseButton_1')
+        Switch154.click()
+        is154NotSuccessful = SwitchTo('pageContent_ClassScheduleUC_dl_coursegroup_PrimarySections_0_SecondarySections_0_SwitchLinkSecondary_2') and SwitchTo('pageContent_ClassScheduleUC_dl_coursegroup_PrimarySections_0_SecondarySections_0_SwitchLinkSecondaryAlt_3')
+
+    if is165NotSuccessful:
+        MyClassSchedule = browser.find_element_by_xpath("//*[@id='ctl05']")
+        MyClassSchedule.click()
+        Switch165 = browser.find_element_by_id('pageContent_CourseList_SwitchCourseButton_2')
+        Switch165.click()
+        is165NotSuccessful = SwitchTo('pageContent_ClassScheduleUC_dl_coursegroup_PrimarySections_0_SecondarySections_0_SwitchLinkSecondaryAlt_1')
+
     logout = browser.find_element_by_xpath("//*[@id='headerTable']/tbody/tr[2]/td[2]/a")
     logout.click()
+
