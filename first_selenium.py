c26492c9a6d94f5bbd1c011e9c90f1694ad9a1ab
Baby Pepper <royxagain@gmail.com>
2016-05-25 00:14:00 -0700

    wrap funciton

--- first_selenium.py
+++ first_selenium.py
@@ -8,10 +8,10 @@ smtpAccount = "royxagain@gmail.com"
 sender = "royxagain@gmail.com"
 receiver = "royxagain@gmail.com"
 count = 5
-spaceLimit160 = 10
-spaceLimit176A = 10
+spaceLimit160 = 5
+spaceLimit176A = 30
 spaceLimit20 = 5
-spaceLimitW12 = 10
+spaceLimitW12 = 30
 
 def sendEmail(message):
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -20,6 +20,20 @@ def sendEmail(message):
     #server.ehlo()
     server.login(smtpAccount, emailPassword)
     server.sendmail(sender,receiver, message)
+
+def checkSpace(browser, quarter, subject, space):
+    FindCourse = browser.find_element_by_xpath("//*[@id='ctl07']")
+    FindCourse.click()
+    browser.find_element_by_xpath(quarter).click()
+    browser.find_element_by_xpath(subject).click()
+    beginSearch = browser.find_element_by_id('pageContent_searchButton')
+    beginSearch.click()
+    return browser.find_element_by_xpath(space).text
+
+def notify(browser, current_value, threashold, msg):
+    if current_value < threashold:
+        sendEmail(msg)
+        browser.close()
 
 
 browser = webdriver.Chrome()
@@ -35,30 +49,26 @@ while 1:
         NetId.send_keys('yanxi')
         password.send_keys(readPassword)
         login.click()
-        FindCourse = browser.find_element_by_xpath("//*[@id='ctl07']")
-        FindCourse.click()
-        browser.find_element_by_xpath("//*[@id='pageContent_quarterDropDown']/option[1]").click()
-        browser.find_element_by_xpath("//*[@id='pageContent_subjectAreaDropDown']/option[18]").click()
-        beginSearch = browser.find_element_by_id('pageContent_searchButton')
-        beginSearch.click()
-        CS160Space = browser.find_element_by_xpath("//*[@id='pageContent_CourseList_PrimarySections_11']/tbody/tr/td/table/tbody/tr[1]/td[7]").text
+        quarter = "//*[@id='pageContent_quarterDropDown']/option[1]"
+        CS = "//*[@id='pageContent_subjectAreaDropDown']/option[18]"
+        cs160 = "//*[@id='pageContent_CourseList_PrimarySections_11']/tbody/tr/td/table/tbody/tr[1]/td[7]"
+        CS160Space = checkSpace(browser, quarter, CS, cs160)
+
         space160 = "CS160Space"
-        CS176ASpace = browser.find_element_by_xpath("//*[@id='pageContent_CourseList_PrimarySections_13']/tbody/tr/td/table/tbody/tr[1]/td[7]").text
+        cs176a = "//*[@id='pageContent_CourseList_PrimarySections_13']/tbody/tr/td/table/tbody/tr[1]/td[7]"
+        CS176ASpace = checkSpace(browser, quarter, CS, cs176a)
+
         space176A = "CS176ASpace"
-        FindCourse = browser.find_element_by_xpath("//*[@id='ctl07']")
-        FindCourse.click()
-        browser.find_element_by_xpath("//*[@id='pageContent_subjectAreaDropDown']/option[23]").click()
-        beginSearch = browser.find_element_by_id('pageContent_searchButton')
-        beginSearch.click()
-        Earth20Space = browser.find_element_by_xpath("//*[@id='pageContent_CourseList_PrimarySections_7_SecondarySections_0']/tbody/tr[1]/td/table/tbody/tr[1]/td[8]").text
+        Earth = "//*[@id='pageContent_subjectAreaDropDown']/option[23]"
+        earth20 = "//*[@id='pageContent_CourseList_PrimarySections_7_SecondarySections_0']/tbody/tr[1]/td/table/tbody/tr[1]/td[8]"
+        Earth20Space = checkSpace(browser, quarter, Earth, earth20)
+
         space20 = "Earch20Space"
-        FindCourse = browser.find_element_by_xpath("//*[@id='ctl07']")
-        FindCourse.click()
-        browser.find_element_by_xpath("//*[@id='pageContent_subjectAreaDropDown']/option[40]").click()
-        beginSearch = browser.find_element_by_id('pageContent_searchButton')
-        beginSearch.click()
-        GeogW12Space = browser.find_element_by_xpath("//*[@id='pageContent_CourseList_PrimarySections_3']/tbody/tr/td/table/tbody/tr[1]/td[7]").text
+        Geology = "//*[@id='pageContent_subjectAreaDropDown']/option[40]"
+        geogw12 = "//*[@id='pageContent_CourseList_PrimarySections_3']/tbody/tr/td/table/tbody/tr[1]/td[7]"
+        GeogW12Space = checkSpace(browser, quarter, Geology, geogw12)
         spaceW12 = "GeogW12Space"
+
         if int(CS160Space) < spaceLimit160:
             msg = "\nHello! " + space160 + " is less than" + str(spaceLimit160)# The /n separates the message from the headers (which we ignore for this example)
             sendEmail(msg)
